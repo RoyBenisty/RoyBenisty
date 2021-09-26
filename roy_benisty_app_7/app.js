251130d41e627d1768c17ba75f93680385300f8b
Roy Benisty <88325080+RoyBenisty@users.noreply.github.com>
2021-09-26 14:58:20 +0300

    excrise 7

---
+++ roy_benisty_app_7/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const sql= require ("./DB/db.js");
+const app = express();
+const path = require("path");
+
+
+
+// parse requests of content-type: application/json
+ app.use(bodyParser.json());
+// parse requests of content-type: application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const publicDirectory = path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+
+
+// simple route
+app.get("/", (req, res) => {
+res.sendFile(path.join(__dirname,"/views/CV2.html"));
+});
+
+// set port, listen for requests 
+app.listen(8000, () => {
+console.log("Server is running on port 8000." );
+});
